7e7a827482bb227ed3cbdc7f7d486424e885c941
mrcrnkovich <mrcrnkovich@gmail.com>
2020-06-26 19:11:28 -0400

    config

--- CRM.py
+++ CRM.py
@@ -5,3 +5,6 @@ from app.models import User, Client
 @crm.shell_context_processor
 def make_shell_context():
     return {}
+
+if __name__ == '__main__':
+    crm.run()
---
+++ config.py
@@ -0,0 +1,35 @@
+# Configuration File
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = "Any_Secret_Key"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://postgres:postgres@localhost:5432/crmDatabase"
+    )
+
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    DEBUG = True
+    RELOAD = True
+    SQLALCHEMY_ECHO = True
+
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+
+app_config = {"development": DevelopmentConfig, "production": ProductionConfig}
